79db992bab5dd6883a37095f571a4f190b11b2de
Eugene Clark <clarkjr2016@gmail.com>
2023-09-11 21:19:43 -0400

    Able to type in an area and get the temp in farenheit back

--- functions/weatherFunctions.js
+++ functions/weatherFunctions.js
@@ -3,7 +3,6 @@ import { callCurrentWeather } from "./apiCall.js";
 async function farenheit(userEnteredLocation) {
   const locationObject = await callCurrentWeather(userEnteredLocation); //placing the returned object in a variable by invoking the callCurrentweather function
   const farenheitTemp = locationObject.current.temp_f; //accessing the farenheit tempature by selecting the appropriate property
-  console.log(farenheitTemp);
   return farenheitTemp;
 }
 
--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-import { callCurrentWeather, weatherStatus } from "./functions/apiCall.js"; // Make sure to add the .js next time lol
+import { farenheit } from "./functions/weatherFunctions.js"; //this function returns the farenheight temperature recorded for the location entered in
 
-weatherStatus("Atlanta, GA");
+const input = document.querySelector("#location"); // this selects the input element
+
+const button = document.querySelector("button"); // this selects the button element
+
+const weatherText = document.querySelector("h1"); // this selects the h2 for the weather to be updated with
+
+button.addEventListener("click", async () => {
+  let userInput = input.value; // assign the current input value to a variable
+  try {
+    const farenheitTemp = await farenheit(userInput); // await for the results off farenheit to be resolved
+    weatherText.innerText = farenheitTemp; // sets the innertext of the H1 element to be the value retured from the farenheit function
+    input.value = "";
+  } catch (err) {
+    console.error(err);
+  }
+  //create a function  that will take in the number data as an argument and chage the inner value of the H2 element to match the temp associated with
+});
